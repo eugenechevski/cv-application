852115c73ca4588a3a488d093e4586b4bab8e307
Eugene Chevski <eugenechevski@proton.me>
2022-12-15 13:45:36 -0500

    removes Input's component input validation

--- src/components/edit-page/Input.tsx
+++ src/components/edit-page/Input.tsx
@@ -3,26 +3,9 @@ import uniqid from "uniqid";
 
 const Input = (props: any) => {
   const { data, updateData, title } = props;
-  const [isValidInput, setValidity] = useState(false);
 
   const handleNewInput = (e: Event) => {
     const target = e.target as HTMLInputElement;
-
-    if (target.value.length > 30) {
-      target.setCustomValidity(
-        `The length of ${(
-          title as string
-        ).toLowerCase()} cannot exceed 30 characters.`
-      );
-      setValidity(false);
-      return;
-    } else if (target.value.length === 0) {
-      target.setCustomValidity(`${title} has to have at least one character.`);
-      setValidity(false);
-    } else {
-      setValidity(true);
-    }
-
     data.current = target.value;
     updateData({ current: target.value });
   };
@@ -40,10 +23,7 @@ const Input = (props: any) => {
         value={data.current}
         type="text"
         onChange={(e: any) => handleNewInput(e)}
-        className={
-          "input input-bordered w-full max-w-xs" +
-          (!isValidInput ? " input-error" : "")
-        }
+        className="input input-bordered w-full max-w-xs"
       />
     </div>
   );
